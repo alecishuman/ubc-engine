f68b767c5c54cddc8cced94de4b1ac7dc753b17c
alecishuman <aasitu@gmail.com>
2024-05-21 22:36:30 -0700

    backend cors

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify;
+from flask_cors import CORS;
 
 # Create a Flask app
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api', methods=['GET'])
 def return_response():
     return jsonify({'data': 'Hello World!'})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
